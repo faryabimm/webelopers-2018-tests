ba21e187e499d624066a50ec1a0ef45f2fd0210f
Mohammadmahdi Faryabi <faryabimohammadmahdi@gmail.com>
2018-11-05 18:04:20 +0330

    reformatting files

--- test_server/app.py
+++ test_server/app.py
@@ -7,9 +7,7 @@ import requests
 from flask import Flask, request, Response
 from timeout_decorator import timeout, TimeoutError
 
-import configuration as config
-import logger
-import tests
+from test_server import configuration as config, logger, tests
 
 app = Flask(__name__)
 
